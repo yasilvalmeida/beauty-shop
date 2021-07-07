fd2227035b48948856eb18c435e21febbe334136
Yasser Almeida <yasilvalmeida@hotmail.com>
2021-07-07 09:15:40 +0000

    Integrate PM to fetch single product

--- services/actions/products.js
+++ services/actions/products.js
@@ -507,6 +507,137 @@ export const getEightProductsWithFilter = (position) => {
 
 export const getSingleProduct = (id) => {
     return (dispatch) => {
+      dispatch(backendLogin());
+      dispatch({ type: SET_PRODUCT_SINGLE_LOADED });
+      const plentyMarketAuthData = JSON.parse(
+        localStorage.getItem("plentyMarketAuthData")
+      );
+      const { accessToken } = plentyMarketAuthData;
+      axios
+        .get(
+          `${process.env.PLENTY_MARKET_API_URL}/items/${id}`,
+          {
+            headers: {
+              Authorization: `Bearer ${accessToken}`,
+            },
+          }
+        )
+        .then(async (res) => {
+          const { data } = res;
+          const images = [];
+          const variants_of_a_products = [];
+          const { id, manufacturerId, createdAt, updatedAt, texts } = data;
+          const {
+            name1,
+            shortDescription,
+            metaDescription,
+            description,
+            technicalData,
+            keywords,
+          } = texts[0];
+          const variationsFetchData = await axios.get(
+            `${process.env.PLENTY_MARKET_API_URL}/items/${id}/variations`,
+            {
+              headers: {
+                Authorization: `Bearer ${accessToken}`,
+              },
+            }
+          );
+          const imagesFetchData = await axios.get(
+            `${process.env.PLENTY_MARKET_API_URL}/items/${id}/images`,
+            {
+              headers: {
+                Authorization: `Bearer ${accessToken}`,
+              },
+            }
+          );
+          const imagesData = imagesFetchData?.data;
+          await imagesData.map((image, j) => {
+            const formats = [];
+            const {
+              cleanImageName,
+              fileType,
+              height,
+              width,
+              size,
+              url,
+              insert,
+              lastUpdate,
+              urlMiddle,
+              urlPreview,
+            } = image;
+            if (url) {
+              formats.push({
+                large: {
+                  url,
+                  mime: fileType,
+                },
+              });
+            }
+            if (urlMiddle) {
+              formats.push({
+                medium: {
+                  url: urlMiddle,
+                  mime: fileType,
+                },
+              });
+            }
+            if (urlPreview) {
+              formats.push({
+                thumbnail: {
+                  url: urlPreview,
+                  mime: fileType,
+                },
+              });
+            }
+            images.push({
+              name: cleanImageName,
+              mime: fileType,
+              height,
+              width,
+              size,
+              url,
+              formats,
+              created_at: insert,
+              updated_at: lastUpdate,
+            });
+          });
+          const variationsData = variationsFetchData?.data?.entries;
+          await variationsData.map((variation, v) => {
+            const { isMain, purchasePrice, availability } = variation;
+            //if (purchasePrice > price) price = purchasePrice;
+            variants_of_a_products.push({
+              main: v === 0 ? true : false,
+              images: images.length > 0 ? [images[0]] : [],
+              price: purchasePrice,
+              quantity: availability,
+            });
+          });
+          await variants_of_a_products.map((vop, v) => {
+            if (v === 0) {
+              vop.main = true;
+            } else {
+              vop.main = false;
+            }
+            //vop.price = price;
+          });
+          dispatch({
+            type: GET_SINGLE_PRODUCT_DATA,
+            payload: {
+              id,
+              name: name1,
+              manufacturerId,
+              images,
+              variants_of_a_products,
+              created_at: createdAt,
+              updated_at: updatedAt,
+              keywords,
+            },
+          });
+        })
+        .catch((err) => dispatch({ type: SET_ERROR, payload: err }));
+    };
+    /* return (dispatch) => {
         dispatch({type: SET_PRODUCT_SINGLE_LOADED});
         axios
             .get(`${process.env.NEXT_PUBLIC_API_URL}/products/${id}`, {
@@ -527,7 +658,7 @@ export const getSingleProduct = (id) => {
                 });
             })
             .catch((err) => dispatch({type: SET_ERROR, payload: err}));
-    };
+    }; */
 };
 
 // export const setSingleProductSelected = (id) => {
